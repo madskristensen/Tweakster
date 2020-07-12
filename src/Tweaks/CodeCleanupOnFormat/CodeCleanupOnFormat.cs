54ecbc10fbe9deb5e2a22f268512bab64716bf80
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-07-11 19:57:02 -0700

    Added Code Cleanup on format document

---
+++ src/Tweaks/CodeCleanupOnFormat/CodeCleanupOnFormat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Tweakster.Tweaks.CodeCleanupOnFormat
+{
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof(CodeCleanupOnFormat))]
+    [ContentType("Csharp")]
+    [ContentType("Basic")]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    public class CodeCleanupOnFormat : ICommandHandler<FormatDocumentCommandArgs>
+    {
+        public string DisplayName => nameof(CodeCleanupOnFormat);
+
+        [Import]
+        private readonly IEditorCommandHandlerServiceFactory _commandService = default;
+
+        public bool ExecuteCommand(FormatDocumentCommandArgs args, CommandExecutionContext executionContext)
+        {
+            if (!Options.Instance.RunCodeCleanupOnFormat)
+            {
+                return false;
+            }
+
+            try
+            {
+                IEditorCommandHandlerService service = _commandService.GetService(args.TextView);
+
+                var cmd = new CodeCleanUpDefaultProfileCommandArgs(args.TextView, args.SubjectBuffer);
+                service.Execute((v, b) => cmd, null);
+            }
+            catch (Exception ex)
+            {
+                Trace.Write(ex);
+            }
+
+            return false;
+        }
+
+        public CommandState GetCommandState(FormatDocumentCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+    }
+}
---
+++ src/Tweaks/CodeCleanupOnFormat/Options.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Tweakster
+{
+    internal partial class Options : BaseOptionModel<Options>
+    {
+        [Category(Category.Editor)]
+        [DisplayName("Code Cleanup on save")]
+        [Description("Determines if Code Cleanup should execute when formatting the document")]
+        [DefaultValue(true)]
+        public bool RunCodeCleanupOnFormat { get; set; } = true;
+    }
+}
