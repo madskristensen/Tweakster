2bc1fa795f3ce44b317809dded4b60220a7b29e8
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-07-12 19:13:18 -0700

    Updated name and icon

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace Tweakster
     internal sealed partial class Vsix
     {
         public const string Id = "0c8bd9fa-77d5-4563-ab57-9e01608c3d04";
-        public const string Name = "Tweakster";
+        public const string Name = "Tweaks";
         public const string Description = @"A collection of minor fixes and tweaks for Visual Studio to reduce the paper cuts and make you a happier developer";
         public const string Language = "en-US";
         public const string Version = "1.0";
