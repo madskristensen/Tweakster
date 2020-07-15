079699bcbfcfcac1f89577c2523163d926c97c2f
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-07-14 19:56:46 -0700

    Moved settings around

--- src/Options/Options.cs
+++ src/Options/Options.cs
@@ -2,6 +2,12 @@
 {
     internal partial class Options : BaseOptionModel<Options>
     {
+        /* ------------------------------------------
+
+        This class is empty, because each tweak folder contain their own partial option class.
+
+        --------------------------------------------*/
+
         private class Category
         {
             public const string General = "General";
--- src/Tweaks/NoStepDebuggingInDesignMode/Options.cs
+++ src/Tweaks/NoStepDebuggingInDesignMode/Options.cs
@@ -4,7 +4,7 @@ namespace Tweakster
 {
     internal partial class Options : BaseOptionModel<Options>
     {
-        [Category(Category.General)]
+        [Category(Category.Editor)]
         [DisplayName("Format on Move Line")]
         [Description("Calls 'Format Selection' when a line is moved up or down using Alt+UP/DOWN shortcuts.")]
         [DefaultValue(true)]
