0b46b989a39a5249be057ed36f7c4eefc67aabd8
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-07-26 02:00:24 +0200

    Updated collapse form for attributes.

--- src/Tweaks/CollapseMemberAttributes/Options.cs
+++ src/Tweaks/CollapseMemberAttributes/Options.cs
@@ -9,5 +9,11 @@ namespace Tweakster
         [Description("It provides the possibility to collapse group of attributes into one collapsed line.")]
         [DefaultValue(true)]
         public bool CollapseMemberAttributes { get; set; } = true;
+
+        [Category(Category.Editor)]
+        [DisplayName("Short form for attribute collapse")]
+        [Description("Display short text (...) for the collapased form of attributes.")]
+        [DefaultValue(true)]
+        public bool CollapseMemberAttributesShortForm { get; set; } = true;
     }
 }
