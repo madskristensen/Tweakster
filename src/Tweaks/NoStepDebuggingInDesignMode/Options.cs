782641af45f267e2381c6c71d5a7fc18c030bccd
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-07-28 08:12:49 -0700

    Flipped boolean text for setting Fixes #9

--- src/Tweaks/NoStepDebuggingInDesignMode/Options.cs
+++ src/Tweaks/NoStepDebuggingInDesignMode/Options.cs
@@ -5,7 +5,7 @@ namespace Tweakster
     internal partial class Options : BaseOptionModel<Options>
     {
         [Category(Category.Debug)]
-        [DisplayName("No debug on F10/F11")]
+        [DisplayName("Start debugging on F10/F11")]
         [Description("Disables F10/F11 until debug mode is entered to stop accidental debugger starts.")]
         [DefaultValue(false)]
         public bool DebugOnF10F11 { get; set; } = false;
