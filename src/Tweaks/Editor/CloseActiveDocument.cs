08baf4a586193a6ce929c62abe0d9743d8515012
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-08-04 15:14:18 -0700

    Changed implementation of closing documents /cc #13

--- src/Tweaks/Editor/CloseActiveDocument.cs
+++ src/Tweaks/Editor/CloseActiveDocument.cs
@@ -1,78 +1,41 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using EnvDTE;
-using EnvDTE80;
+using System.Windows;
+using System.Windows.Input;
 using Microsoft;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 namespace Tweakster
 {
-    internal sealed class CloseActiveDocument : IOleCommandTarget
+    internal sealed class CloseActiveDocument
     {
-        private readonly DTE2 _dte;
-
-        private static Guid _cmdGuid = new Guid("{1496A755-94DE-11D0-8C3F-00C04FC2AAE2}");
-        private static readonly uint _cmdSelectWordId = 0x5a;
-        private static readonly uint _cmdNextKeyId = 0x4d1;
-
-        private CloseActiveDocument(DTE2 dte)
-        {
-            _dte = dte;
-        }
+        private static IVsUIShell _uiShell;
 
         public static async Task InitializeAsync(AsyncPackage package)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
-            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Assumes.Present(commandService);
+            _uiShell = await package.GetServiceAsync<SVsUIShell, IVsUIShell>();
+            Assumes.Present(_uiShell);
 
-            var dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
-            var pct = await package.GetServiceAsync(typeof(SVsRegisterPriorityCommandTarget)) as IVsRegisterPriorityCommandTarget;
-            Assumes.Present(pct);
-
-            var interceptor = new CloseActiveDocument(dte);
-            pct.RegisterPriorityCommandTarget(0, interceptor, out _);
+            Application.Current.MainWindow.KeyUp += OnKeyUp;
         }
 
-        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
-        {
-            return (int)Microsoft.VisualStudio.OLE.Interop.Constants.OLECMDERR_E_NOTSUPPORTED;
-        }
-
-        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        private static void OnKeyUp(object sender, KeyEventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            if (pguidCmdGroup == _cmdGuid && (nCmdID == _cmdSelectWordId || nCmdID == _cmdNextKeyId))
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control &&
+                Options.Instance.CloseTabOnControlW &&
+                e.Key == Key.W)
             {
-                if (IsDocumentWindowActive())
-                {
-                    Command cmd = _dte.Commands.Item("File.Close");
-
-                    if (cmd != null && cmd.IsAvailable)
-                    {
-                        _dte.ExecuteCommand("File.Close");
-                        return VSConstants.S_FALSE;
-                    }
-                }
+                e.Handled = true;
+                Guid guid = typeof(VSConstants.VSStd97CmdID).GUID;
+                var id = (uint)VSConstants.VSStd97CmdID.FileClose;
+                _uiShell.PostExecCommand(guid, id, 0, null);
             }
-
-            return (int)Microsoft.VisualStudio.OLE.Interop.Constants.MSOCMDERR_E_FIRST;
-        }
-
-        private bool IsDocumentWindowActive()
-        {
-            return Options.Instance.CloseTabOnControlW &&
-                    (
-                        _dte.ActiveWindow?.Type == vsWindowType.vsWindowTypeCodeWindow ||
-                        _dte.ActiveWindow?.Type == vsWindowType.vsWindowTypeDesigner ||
-                        _dte.ActiveWindow?.Type == vsWindowType.vsWindowTypeDocument
-                    );
         }
     }
 }
