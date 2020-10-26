f259807103b9b8e2e221d485137d2a58f78ece08
Steven Bone <sbone@pobox.com>
2020-10-26 11:16:28 -0400

    Implement #60. Adds capability for a property defined in BaseOptionModel to be attributed to override the default collection and data type where the setting is stored in the Visual Studio SettingsStore. This functionality was then used to implement getting and setting the General\U2DCheckVerbosity setting. (#61)

--- src/Tweaks/Build/Options.cs
+++ src/Tweaks/Build/Options.cs
@@ -17,5 +17,22 @@ namespace Tweakster
         [Description("Specifies the obj and bin folders should be deleted when running the Clean command.")]
         [DefaultValue(true)]
         public bool DeleteOuputArtifactsOnClean { get; set; } = true;
+
+        /// <summary>   If Enabled, the Build Output Windows will output the reason why a project is
+        ///             being built.</summary>
+        /// <remarks>   Ultimately, this will set the DWORD registry value <c>U2DCheckVerbosity</c> in the 
+        ///             Visual Studio registry hive under the <c>General</c> key. DO NOT alter the
+        ///             property name as it reflects the registry value that is set. Note the
+        ///             <see cref="OverrideCollectionNameAttribute"/> and <see cref="OverrideDataTypeAttribute"/>
+        ///             have been applied so the value is loaded/saved in the appropriate place and is of
+        ///             the appropriate type. <a href="https://github.com/madskristensen/Tweakster/issues/60">
+        ///             Issue documentation</a> </remarks>
+        [Category(_build)]
+        [DisplayName("Up-To-Date Check Verbose")]
+        [Description("If Enabled, the Build Output Windows will output the reason why a project is being built.")]
+        [DefaultValue(false)]
+        [OverrideCollectionName("General")]
+        [OverrideDataType(SettingDataType.Bool)]
+        public bool U2DCheckVerbosity { get; set; } = false;
     }
 }
