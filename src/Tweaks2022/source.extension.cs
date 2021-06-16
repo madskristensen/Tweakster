12c1bb44e7abc9e37b599aee61f8bbb436ad0dab
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-06-16 15:40:38 -0700

    Updated product id

--- src/Tweaks2022/source.extension.cs
+++ src/Tweaks2022/source.extension.cs
@@ -7,7 +7,7 @@ namespace Tweakster
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "0c8bd9fa-77d5-4563-ab57-9e01608c3d04";
+        public const string Id = "0c8bd9fa-77d5-4563-ab57-9e01608c3d05";
         public const string Name = "Tweaks 2022";
         public const string Description = @"A collection of minor fixes and tweaks for Visual Studio to reduce the paper cuts and make you a happier developer";
         public const string Language = "en-US";
