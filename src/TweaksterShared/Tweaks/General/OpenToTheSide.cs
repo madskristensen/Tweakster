d5ffb2b3db0e8fe49b8cacad8687e5889556214a
Mads Kristensen <madskristensen@users.noreply.github.com>
2022-04-22 09:10:41 -0700

    Removed the Alt+click feature for opening file on the side Fixes #100

--- src/TweaksterShared/Tweaks/General/OpenToTheSide.cs
+++ src/TweaksterShared/Tweaks/General/OpenToTheSide.cs
@@ -8,48 +8,18 @@ namespace Tweakster
     [Command(PackageGuids.guidCommandsString, PackageIds.OpenToTheSide)]
     internal sealed class OpenToTheSide : BaseCommand<OpenToTheSide>
     {
-        private bool _handled;
-        
-        protected override Task InitializeCompletedAsync()
-        {
-            VS.Events.DocumentEvents.Opened += OnOpened;
-
-            return base.InitializeCompletedAsync();
-        }
-
-        private void OnOpened(string obj)
-        {
-            if (!_handled && (Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt)))
-            {
-                ThreadHelper.JoinableTaskFactory.StartOnIdle(async () =>
-                {
-                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                    await VS.Commands.ExecuteAsync("Window.NewVerticalTabGroup");
-                    
-                }, VsTaskRunContext.UIThreadNormalPriority).FireAndForget();
-            }
-        }
-
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            try
-            {
-                _handled = true;
-                SolutionItem item = await VS.Solutions.GetActiveItemAsync();
+            SolutionItem item = await VS.Solutions.GetActiveItemAsync();
 
-                if (item is PhysicalFile file)
+            if (item is PhysicalFile file)
+            {
+                DocumentView docView = await VS.Documents.OpenAsync(file.FullPath);
+
+                if (docView != null)
                 {
-                    DocumentView docView = await VS.Documents.OpenAsync(file.FullPath);
-
-                    if (docView != null)
-                    {
-                        await VS.Commands.ExecuteAsync("Window.NewVerticalTabGroup");
-                    }
+                    await VS.Commands.ExecuteAsync("Window.NewVerticalTabGroup");
                 }
-            }
-            finally
-            {
-                _handled = false;
             }
         }
     }
